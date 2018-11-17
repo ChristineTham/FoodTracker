d01b6301d04d73d13028623da31d202090acc2bc
Christine Tham <chris.tham@hellotham.com>
2018-11-17 14:27:41 +1100

    Checkpoint Created Table View

---
+++ FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by Chris Tham on 17/11/18.
+//  Copyright © 2018 Hello Tham. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
